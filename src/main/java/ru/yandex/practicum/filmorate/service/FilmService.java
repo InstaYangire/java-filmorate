a327ebb73ea42eeeb8e0e3edaa5e14440c304934
Byswork <byswork@yandex.ru>
2025-09-20 09:44:19 +0300

    Add common films

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -12,7 +12,6 @@ import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
-import ru.yandex.practicum.filmorate.storage.film.FilmDbStorage;
 
 import java.util.Comparator;
 import java.util.List;
