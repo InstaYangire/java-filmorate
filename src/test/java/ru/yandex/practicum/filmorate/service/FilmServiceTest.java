1f35c517414364462090cc3c6fb321fca98d1734
Byswork <byswork@yandex.ru>
2025-09-24 00:15:47 +0300

    Add recommendations

--- src/test/java/ru/yandex/practicum/filmorate/service/FilmServiceTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/service/FilmServiceTest.java
@@ -2,16 +2,12 @@ package ru.yandex.practicum.filmorate.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.DirectorStorage;
-import ru.yandex.practicum.filmorate.storage.FilmStorage;
-import ru.yandex.practicum.filmorate.storage.UserStorage;
 import ru.yandex.practicum.filmorate.storage.film.DirectorDbStorage;
 import ru.yandex.practicum.filmorate.storage.film.GenreDbStorage;
 import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
@@ -26,7 +22,6 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FilmServiceTest {
-    private static final Logger log = LoggerFactory.getLogger(FilmServiceTest.class);
     private FilmService filmService;
     private UserService userService;
 
