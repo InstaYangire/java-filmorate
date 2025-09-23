07ad7465e1a5d578f3283b2b38e6de5d3a224129
Byswork <byswork@yandex.ru>
2025-09-23 16:22:53 +0300

    Add recommendations

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -5,7 +5,6 @@ import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Director;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.Friendship;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
 
